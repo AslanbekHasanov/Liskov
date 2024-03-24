1af5e3ccd0a511c6bd053a573163f91cfb7df286
AslanbekHasanov <learneraslanbek1220@gmail.com>
2024-03-24 17:55:21 +0500

    Write Document

---
+++ Liskov/Service/Document.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Liskov.Service
+{
+    internal class Document
+    {
+        private string date = "";
+        private readonly string path;
+
+        public Document(string path)
+        {
+            this.path = path;
+            this.CheckExsist();
+        }
+
+        public void Open()
+        {
+            date = File.ReadAllText(path);
+        }
+
+        private void CheckExsist()
+        {
+            if (File.Exists(path) is false)
+            {
+                File.Create(path).Close();
+            }
+        }
+
+    }
+}
